c58b85834336c98f4c232eb91cfe8cb16995e1fd
rails -v <netagonen@gmail.com>
2016-12-04 15:41:15 +0100

    fortuneteller with user input validation

--- fortuneteller.rb
+++ fortuneteller.rb
@@ -1,48 +1,80 @@
-$prompt = "> "
+def ask_the_user
+  print "> "
+  gets.chomp
+end
 
-def yes_to_fortune
+def get_y_n?
+  while true
+    answer = ask_the_user.capitalize
+    if answer == "Y"
+      return true
+    elsif answer == "N"
+      return false
+    else
+      puts "This is an ERROR, you typed #{answer}, please type y or n."
+    end
+  end
+end
+
+def get_num
+  while true
+    answer = ask_the_user
+    answer_as_i = answer.to_i
+    answer_as_s = answer_as_i.to_s
+    if answer == answer_as_s && answer_as_i >= 0
+      return answer_as_i
+    else
+      puts "This is an ERROR, you typed #{answer}, please type a positive integer"
+    end
+  end
+end
+
+
+def telling_fortune
 
   puts "Great, now I would have to ask you more questions!"
-  puts "Which continent are you from?"
-  print $prompt
-  continent = gets.chomp.capitalize
-  case continent
-  when "Asia", "Africa"
-    fortune_cont = "you would travel a lot in your lifetime with orgenized travel agencies"
-  when "America", "Antarctica"
-    fortune_cont = "you are not gonna travel a lot around the world and you won't have any problems with it, cause you really love your own continent"
-  when "Europe", "Australia"
-    fortune_cont - "you would travel a lot around the worled, mainly with unconvetional means of transportation"
-  else
-    puts "This is an ERROR, you typed #{continent}, please type one of the following: Africa, America, Antarctica, Asia, Australia, Europe."
-    print $prompt
-    continent = gets.chomp.capitalize
+
+  correct = false
+  while correct == false
+    puts "Which continent are you from?"
+    continents = ["Asia", "Africa", "America", "Antarctica", "Europe", "Australia"]
+    continent = ask_the_user.capitalize
+    if continents.include?(continent)
+      case continent
+      when "Asia", "Africa"
+        fortune_cont = "you would travel a lot in your lifetime with orgenized travel agencies"
+        correct = true
+      when "America", "Antarctica"
+        fortune_cont = "you are not gonna travel a lot around the world and you won't have any problems with it, cause you really love your own continent"
+        correct = true
+      when "Europe", "Australia"
+        correct = fortune_cont - "you would travel a lot around the worled, mainly with unconvetional means of transportation"
+        correct = true
+      end
+    else
+      puts "This is an ERROR, you typed #{continent}, please type one of the following: Africa, America, Antarctica, Asia, Australia, Europe."
+    end
   end
 
-  puts "Pick a number from 0-10"
-  print $prompt
-  number = gets.chomp.to_i
+
+  puts "Pick a number from 1-10"
+  number = get_num
   case number
-  when 0..2
+  when 1..3
     num_fortune = "you are going to have an amazing experience in a week"
-  when 3..6
+  when 4..6
     num_fortune = "you are going to have an amazing experience in a year"
   when 7..10
     num_fortune = "you are going to have an amazing experience in 10 years"
-  else
-    puts "This is an ERROR, you typed #{number}, please tipe a number between 0-10"
   end
 
   puts "Do you like ice creame? (y/n)"
-  print $prompt
-  ice_cre = gets.chomp.capitalize
+  ice_cre = get_y_n?
   case ice_cre
-  when "Y"
+  when true
     ice_fortune = "you are going to heaven!! yeepee :)"
-  when "N"
+  when false
     ice_fortune = "you are going to hell, boo hoo :( "
-  else
-    puts "This is an ERROR, you typed #{ice_cre}, please type y or n."
   end
 
   puts "Let me think......\nOK #{$name}, your fortune tells me that #{fortune_cont}; #{num_fortune} and... #{ice_fortune}"
@@ -52,20 +84,28 @@ end
 puts "Welcome to the fortune teller game. I will tell your fortune"
 
 puts "What is your name?"
-print $prompt
-$name = gets.chomp.capitalize
-puts "Your name is #{$name}"
+correct = false
+while correct == false
+  $name = ask_the_user.capitalize
+  puts "Your name is #{$name}, is this right (y/n)?"
+  name = get_y_n?
+  if name == true
+    correct = true
+    puts "Great, let's move on"
+  else
+    puts "Ok, type your name again"
+  end
+end
 
 puts "#{$name} how old are you?"
-print $prompt
-age = gets.chomp.to_i
+age = get_num
 puts "#{$name}, you are #{age} years old, very nice!"
 
+
 puts "#{$name}, would you like me to tell you your fortune (y/n)?"
-print $prompt
-answer = gets.chomp.capitalize
-if answer == "Y"
-  yes_to_fortune
-else answer == "N"
+answer = get_y_n?
+ if answer
+  telling_fortune
+else
   puts "OK then #{$name}, see you next time"
 end
